4286005f9171a810c9a08554771de9fe21258fdd
tiff-wang <wangtiffany@gmail.com>
2017-07-22 09:57:04 -0400

    added configuration.js in vc

---
+++ configuration.js
@@ -0,0 +1,12 @@
+exports.database = {
+    userName: 'mtsateam',
+    password: '@Mttesaam13579',
+    server: 'mtsa.database.windows.net',
+    options: {
+        database: 'mtsa_website',
+        encrypt: "true",
+        rowCollectionOnDone: 'true'
+    }
+}
+
+exports.googleAPIKey = 'AIzaSyCjA2rmHtw5ViJC2msJMq4o00S6JzT8W_w';
