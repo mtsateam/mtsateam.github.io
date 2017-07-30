e3223a791ebb80561ddfcf51033e7f2b00d94ac1
tiff-wang <wangtiffany@gmail.com>
2017-07-30 13:35:15 -0400

    fixed problem with about-us

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,15 +11,6 @@ import { NewsBannerComponent } from './news-banner/news-banner.component';
 
 
 const ROUTES: Routes = [
-  //full : makes sure the path is absolute path
-  // { path: '', redirectTo: 'welcome-page' , pathMatch: 'full'},
-  // { path: 'welcome-page', loadChildren: './welcome-page/welcome-page.module#WelcomePageModule' },
-  // { path: 'about-us', loadChildren: './about-us/about-us.module#AboutUsModule' },
-  // { path: 'events', loadChildren: './events/events.module#EventsModule' },
-  // { path: 'execs', loadChildren: './execs/execs.module#ExecsModule' },
-  // { path: 'sponsors', loadChildren: './sponsors/sponsors.module#SponsorsModule' },
-
-
   { path: '', component: WelcomePageComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'news-banner', component: NewsBannerComponent },
