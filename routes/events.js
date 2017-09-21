59a93c59503e2cdc53e10fa219a0408b2fc5ab70
tiff-wang <wangtiffany@gmail.com>
2017-09-20 21:40:42 -0400

    changed query of upcoming to be with respect to end date

--- routes/events.js
+++ routes/events.js
@@ -4,14 +4,14 @@ var connection = require('../connection/database')
 
 //Get all events
 router.get('/', function(req, res, next){
-  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 107) AS EndTime FROM Events ORDER BY CreationDate;", function(db){
+  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 108) AS EndTime FROM Events ORDER BY CreationDate;", function(db){
     res.send(db);
   })
 });
 
 //Get Thumbnail Events
 router.get('/thumbnail', function(req, res, next){
-  connection.executeQuery("SELECT TOP 4 *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 107) AS EndTime FROM Events ORDER BY CreationDate;", function(db){
+  connection.executeQuery("SELECT TOP 4 *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 108) AS EndTime FROM Events ORDER BY CreationDate;", function(db){
     res.send(db);
   })
 });
@@ -19,7 +19,7 @@ router.get('/thumbnail', function(req, res, next){
 //Get all events after Today
 //TODO change to get events after today
 router.get('/upcoming', function(req, res, next){
-  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 107) AS EndTime FROM Events WHERE Start>GETDATE();", function(db){
+  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 108) AS EndTime FROM Events WHERE Fin>GETDATE();", function(db){
     res.send(db);
   })
 });
@@ -28,7 +28,7 @@ router.get('/upcoming', function(req, res, next){
 //Get Event by ID (Name)
 router.get('/:id', function(req, res, next){
   event_id = req.params.id;
-  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 107) AS EndTime FROM Execs WHERE Name='" + event_id + "';", function(db){
+  connection.executeQuery("SELECT *, CONVERT(varchar(50), Start, 107) AS StartDate, CONVERT(varchar(50), Start, 108) AS StartTime, CONVERT(varchar(50), Fin, 107) AS EndDate, CONVERT(varchar(50), Fin, 108) AS EndTime FROM Execs WHERE Name='" + event_id + "';", function(db){
     res.send(db);
   })
 });
